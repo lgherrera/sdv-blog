7be4c2475d6574a454eba158fce584be803e4891
Luis Gubler <luis@zgroup.cl>
2026-05-22 17:59:19 -0400

    fix: remove broken type import from sanity client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -2,7 +2,6 @@
 
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const projectId = "c39tb4ml";
 export const dataset = "production";
@@ -17,6 +16,7 @@ export const client = createClient({
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: SanityImageSource) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function urlFor(source: any) {
   return builder.image(source);
 }
